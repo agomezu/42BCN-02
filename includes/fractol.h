6679a2c5e567b7ecbd2fa70ec11b2780dd49eabc
agomez-u <agomez-u@student.42barcelona.com>
2023-07-09 21:56:49 +0200

    first directories

---
+++ includes/fractol.h
@@ -0,0 +1,18 @@
+
+#ifndef FRACTOL_H
+
+# define FRACTOL_H
+
+# define WIDTH		1000
+# define HEIGHT		1000
+
+# define THREADS	10
+
+# include "libft.h"
+# include <math.h>
+# include <mlx.h>
+# include <stdint.h>
+
+// Color
+// channel - alpha, red, green, blue color channels
+ 
---
+++ sources/fractol.c
@@ -0,0 +1,6 @@
+#include "fractol.h"
+
+int main(int argc, char **argv[])
+{
+
+}
